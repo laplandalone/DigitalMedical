84650dd453fe05af671abbc8e50bba72c4e8e8a3
laplandalone <sunnyrhq@163.com>
2014-06-05 13:53:19 +0800

    h

---
+++ src/com/health/digitalmedical/view/user/MyHealthActivity.java
@@ -0,0 +1,30 @@
+package com.health.digitalmedical.view.user;
+
+import android.os.Bundle;
+
+import com.health.digitalmedical.BaseActivity;
+import com.health.digitalmedical.R;
+
+public class MyHealthActivity extends BaseActivity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_my_health);
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initValue() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
