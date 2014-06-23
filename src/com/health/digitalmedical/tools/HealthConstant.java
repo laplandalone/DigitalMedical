9f2716105a28fe8744ea7d6e687e321aebb82e26
laplandalone <sunnyrhq@163.com>
2014-06-23 21:27:43 +0800

    all

--- src/com/health/digitalmedical/tools/HealthConstant.java
+++ src/com/health/digitalmedical/tools/HealthConstant.java
@@ -5,8 +5,8 @@ import android.os.Environment;
 public class HealthConstant
 {
 	//133.0.179.151   61.183.0.35:7110  61.183.0.37:7170  
-	public static final String URL ="http://192.168.137.1:7001/mobile.htm?method=axis";
-//	public static final String URL ="http://58.53.209.120:9100/mobile.htm?method=axis";
+//	public static final String URL ="http://192.168.137.1:7001/mobile.htm?method=axis";
+	public static final String URL ="http://58.53.209.120:9100/mobile.htm?method=axis";
 	
 	//public static final String URL ="http://61.183.0.35:7100/realNameRegister/mobile.htm?method=axis";
 //	public static final String UPLOAD_URL = "http://192.168.137.1:7001/mobile.htm";
