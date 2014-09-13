844de145f93aa8cc44a5effa8ff97acbfad67401
Lapland_Alone <Lapland_Alone@192.168.1.8>
2014-09-13 23:23:06 +0800

    all

--- src/com/health/digitalmedical/tools/HealthConstant.java
+++ src/com/health/digitalmedical/tools/HealthConstant.java
@@ -8,8 +8,8 @@ public class HealthConstant
 {
 // 	public static final String URL ="http://192.168.137.1:7001/mobile.htm?method=axis";
 //	public static final String URL ="http://58.53.209.120:9100/mobile.htm?method=axis";
-//	public static final String URL ="http://58.53.209.107:10821/mobile.htm?method=axis";
-	public static final String URL ="http://hiseemedical.com:10821/mobile.htm?method=axis";
+	public static final String URL ="http://58.53.209.107:10821/mobile.htm?method=axis";
+//	public static final String URL ="http://hiseemedical.com:10821/mobile.htm?method=axis";
 	
 //	public static final String UPLOAD_URL = "http://192.168.137.1:7001/fileUpload";
 //	public static final String UPLOAD_URL = "http://58.53.209.120:9100/fileUpload";
@@ -20,7 +20,7 @@ public class HealthConstant
 	
 	public static final String Download_path = Environment.getExternalStorageDirectory().getPath() + "/health/download/";
 	
-	public static final String imgUrl = "http://hiseemedical.com:10821/ImgWeb/photo/";
+	public static final String imgUrl = "http://58.53.209.107:10821/ImgWeb/photo/";
 
 	@SuppressWarnings("rawtypes")
 	public static final Enum WECHAT = SHARE_MEDIA.WEIXIN;  //微信分享名称
